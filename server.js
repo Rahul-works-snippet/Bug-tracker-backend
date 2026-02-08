5771c41c47fb1b92e0a6bcfd1529a8196d1ec861
Rahul-works-snippet <rahulitsme2022@gmail.com>
2026-02-08 22:31:16 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("API running...");
+});
+
+app.listen(5000, ()=>{
+    console.log("Server started on port 5000");
+});
